68a7d2ccef81d8b303bb5e9ada022d6b2c8d0032
Jose Risomar Sousa <jrsousa2@hotmail.com>
2025-01-03 21:31:04 -0500

    Fix issues

---
+++ GitResume.bat
@@ -0,0 +1,23 @@
+@echo off
+REM Check if the argument %1 is provided
+
+IF "%~1"=="" (
+    echo Rerun code with commit description
+) ELSE (
+    cd D:\resumes
+    set GIT_TRACE=1
+    echo FORCE A RE-READING OF THE .gitignore list
+    git rm -r --cached .
+    git add .
+    git branch -M main
+    git commit -m %1
+    REM COMMENTED OUT CODE FORCES A PUSH
+    REM It loses track of remote updates
+    REM git push origin main --force
+    git push origin main
+    REM echo sem nada mostra o echo status
+    echo .
+    echo .
+    echo VIEWS IF BATCH SUCCEEDED
+    git log -n 1
+)
