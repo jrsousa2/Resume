e82d44088f94bf7f36bf5e66d582baf735cbc814
Jose Risomar Sousa <jrsousa2@hotmail.com>
2026-06-09 22:02:40 -0300

    Updt

--- GitRes.bat
+++ GitRes.bat
@@ -1,6 +1,9 @@
 @echo off
 REM Check if the argument %1 is provided
 
+REM SAVE CUR DIR
+set CURDIR=%CD%
+
 IF "%~1"=="" (
     echo Setting description to Updt
     echo .
@@ -9,28 +12,26 @@ IF "%~1"=="" (
   set Desc=%1
 )
 
-rem IF "%~1"=="" (
-rem    echo Rerun code with commit description
-rem ) ELSE (
-    cd D:\Resumes
-    set GIT_TRACE=1
-    echo FORCE A RE-READING OF THE .gitignore list IF IT CHANGES
-    REM git rm -r --cached .
-    REM adds new + modified files
-    REM git add .
-    REM ADDS EVERYTHING
-    git add -A
-    REM BELOW MAY NOT BE NEEDED ANYMORE
-    REM git branch -M main
-    git commit -m %Desc%
-    REM COMMENTED OUT CODE FORCES A PUSH
-    REM It loses track of remote updates
-    REM git push origin main --force
-    git push origin main
-    REM echo sem nada mostra o echo status
-    echo.
-    echo.
-    echo VIEWS IF BATCH SUCCEEDED
-    REM USING "git log -n 1" IS NOT TOO USEFUL
-    git log -1 --name-status 
-rem )
+ cd D:\Resumes
+ set GIT_TRACE=1
+ echo FORCE A RE-READING OF THE .gitignore list IF IT CHANGES
+ REM git rm -r --cached .
+ REM adds new + modified files
+ REM git add .
+ REM ADDS EVERYTHING
+ git add -A
+ REM BELOW MAY NOT BE NEEDED ANYMORE
+ REM git branch -M main
+ git commit -m %Desc%
+ REM COMMENTED OUT CODE FORCES A PUSH
+ REM It loses track of remote updates
+ REM git push origin main --force
+ git push origin main
+ REM echo sem nada mostra o echo status
+ echo.
+ echo.
+ echo VIEWS IF BATCH SUCCEEDED
+ REM USING "git log -n 1" IS NOT TOO USEFUL
+ git log -1 --name-status 
+ cd %CURDIR%
+ echo.
